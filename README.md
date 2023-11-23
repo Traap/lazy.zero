946465e4de2059feda325a4bb5a5c0a364e85f0a
Traap <gary.a.howard@mac.com>
2023-11-23 13:59:04 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+Traap's lazy.nvim and lsp.zero.nvim configuration.
+
+### Credits
+* [Folke Lamaitre](https://github.com/folke)
+* [lazy.nvim](https://github.com/folke/lazy.nvim)
+* [Heiker VonKeikemen](https://github.com/VonHeikemen)
+* [lsp-zero.nvim](https://github.com/VonHeikemen/lsp-zero.nvim)
+
+### Installation
+```
+clone https://github.com/Traap/lazy.nvim ~/.config/zero
+
+NVIM_APPNAME=zero nvim
+```
+
+### Extra Credit
+Use [nvims](https://github.com/Traap/dotfiles/blob/master/bash/bin/nvims) to safely experiment with multiple Neovim configurations.
---
+++ init.lua
@@ -0,0 +1,91 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+-- Auto-install lazy.nvim if not present
+if not vim.loop.fs_stat(lazypath) then
+  print('Installing lazy.nvim....')
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+  print('Done.')
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Minimal Neovim settings (listed alphebetically).
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.numberwidth = 3
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 250
+vim.g.mapleader = [[ ]]
+vim.g.maplocalleader = [[ ]]
+
+-- Lazy.nvim setup.
+require('lazy').setup({
+  {'folke/tokyonight.nvim'},
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+
+	{ -- LSP Support
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    lazy = true,
+    config = false,
+  },
+
+	{ -- LSP Config
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+    }
+  },
+
+	{ -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      {'L3MON4D3/LuaSnip'}
+    },
+  },
+
+	{ -- Use which-key to show lsp_zero default_keymaps
+    'folke/which-key.nvim',
+		event = "VeryLazy",
+--		keys = {
+--			{"<leader>wk", "WhichKey", "Toggle Whichkey"},
+--		},
+    config = function()
+      require('which-key').setup(
+			  {plugins = {spelling = true}}
+			)
+    end,
+  },
+})
+
+-- A plesant colorcheme.
+vim.cmd.colorscheme("tokyonight-night")
+
+-- Use lsp_zero to manage lsp attachments.
+local lsp_zero = require('lsp-zero')
+lsp_zero.on_attach(function(_, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- Setup Mason and Mason-Config.
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
+
+-- Configure lua_ls
+local lua_opts = lsp_zero.nvim_lua_ls()
+require('lspconfig').lua_ls.setup(lua_opts)
