fe1c85c83c6b6816ef92f45db7940b7395d8a4c9
Traap <gary.a.howard@mac.com>
2023-11-23 17:42:50 -0800

    Bootstrap Traap style.

--- init.lua
+++ init.lua
@@ -2,18 +2,16 @@ local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 -- Auto-install lazy.nvim if not present
 if not vim.loop.fs_stat(lazypath) then
-  print('Installing lazy.nvim....')
   vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
   })
-  print('Done.')
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 -- Minimal Neovim settings (listed alphebetically).
 vim.opt.mouse = "a"
