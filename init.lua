c184ee207e5c5e2e6bf3f3025b23124ecfc9f32e
Traap <gary.a.howard@mac.com>
2023-12-17 20:07:09 -0800

    Apply lua_ls config option recommended by VonHeikemen.
    https://www.reddit.com/r/neovim/comments/18kt04f/neovim_lazynvim_lspzero_easy_to_use_and_setup/

--- init.lua
+++ init.lua
@@ -114,9 +114,9 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   handlers = {
     lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
   },
 })
-
--- Configure lua_ls
-local lua_opts = lsp_zero.nvim_lua_ls()
-require('lspconfig').lua_ls.setup(lua_opts)
